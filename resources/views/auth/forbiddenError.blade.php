d3155396c9e083c9e8a49562a872df94251c73b4
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-05-03 08:17:26 +0700

    TraceEdu title

--- resources/views/auth/forbiddenError.blade.php
+++ resources/views/auth/forbiddenError.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Star Admin2 </title>
+    <title>TraceEdu</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('TemplateAdmin/src/assets/vendors/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('TemplateAdmin/src/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
