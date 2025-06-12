61baa38f72a2f822282f3d51e44452eab476b2c5
Keysha Arindra Fabian <arindrakeysha@gmail.com>
2025-06-12 12:31:46 +0700

    add bs

--- resources/views/profesi/confirm.blade.php
+++ resources/views/profesi/confirm.blade.php
@@ -23,7 +23,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Hapus Data Profesi</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
